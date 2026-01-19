6cb4e1b762df2bebff9ec4499a473d951e3c96f7
Ignacio Martinoli <ignamartinoli@protonmail.com>
2026-01-18 22:06:37 -0300

    cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.router import api_v1_router
 
 app = FastAPI(title="FastAPI Clean Architecture CRUD + Auth + Transactions")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # add your Railway frontend domain later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_v1_router, prefix="/api/v1")
 
 
